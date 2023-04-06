d8ec71d0662822057adaff31a781b64299839bcd
Guilherme Magro <guilherme.magro@hotmail.com>
2023-04-05 22:36:01 -0300

    Create dots and clock hand animations

--- app/src/main/java/com/guilhermemagro/myplayground/ui/components/ClockAnimated.kt
+++ app/src/main/java/com/guilhermemagro/myplayground/ui/components/ClockAnimated.kt
@@ -1,6 +1,8 @@
 package com.guilhermemagro.myplayground.ui.animations
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
@@ -10,6 +12,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,9 +24,13 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.layout.onGloballyPositioned
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
-private fun calculateClockHandLength(maxHeight: Float, currentHour: Int): Float {
-    val stepHeight = maxHeight / 12
+private fun calculateClockHandLength(stepHeight: Float, currentHour: Int): Float {
     // Height decreases first 360 deg, then increases again
     val stepsNumber = if (currentHour < 12) {
         11 - currentHour
@@ -33,8 +40,12 @@ private fun calculateClockHandLength(maxHeight: Float, currentHour: Int): Float 
     return stepHeight * stepsNumber
 }
 
+private fun calculateAssembleDistance(stepHeight: Float, currentHour: Int): Float =
+    stepHeight * (23 - currentHour)
+
 @Composable
 fun ClockAnimated(
+    modifier: Modifier = Modifier,
     backgroundColor: Color = Color.Black,
     duration: Int = 6000,
 ) {
@@ -50,10 +61,57 @@ fun ClockAnimated(
         )
     )
 
-    val currentHour by remember(animationAngle) {
-        derivedStateOf { animationAngle.toInt() / 30 }
+    // Start calculation each time the animationAngle changes.
+    val assembleValue = remember(animationAngle) {
+        // We only need this animation for second rotation
+        if (animationAngle >= 360) {
+            // Reversed linear interpolation between 0..30 degrees, transformed into 0..1
+            (animationAngle % 30) / 30
+        } else {
+            -1f
+        }
     }
+
+    var currentHour by remember { mutableStateOf(0) }
+
+    val currentHourChannel = remember { Channel<Int>(12, BufferOverflow.DROP_OLDEST) }
+    val currentHourFlow = remember(currentHourChannel) {
+        currentHourChannel.receiveAsFlow()
+    }
+
+    LaunchedEffect(animationAngle) {
+        // Add hour calculation inside of a launchEffect
+        val newCurrentHour = animationAngle.toInt() / 30
+
+        if (newCurrentHour != currentHour) {
+            currentHour = newCurrentHour
+            currentHourChannel.trySend(currentHour)
+        }
+    }
+
     val hours = remember { List(12) { it } }
+
+    val disassembleAnimations = remember { hours.map { Animatable(1f) } }
+
+    // Assume that duration is 1/12th of the whole duration, which equals to the length of 2 hours.
+    // It can be longer or shorter if necessary
+    val disassembleDuration = duration / 12
+
+    LaunchedEffect(currentHourFlow) {
+        currentHourFlow.collectLatest {
+            // Launch each animation asynchronously
+            launch {
+                if (currentHour < 12) {
+                    disassembleAnimations[currentHour].snapTo(0f)
+                    // Set a tween spec with LinearOutSlowInEasing
+                    disassembleAnimations[currentHour].animateTo(
+                        1f,
+                        tween(durationMillis = disassembleDuration, easing = LinearOutSlowInEasing)
+                    )
+                }
+            }
+        }
+    }
 
     val dotsVisibility = remember(currentHour) {
         hours.map { hour ->
@@ -67,7 +125,7 @@ fun ClockAnimated(
 
     var strokeWidth by remember { mutableStateOf(0f) }
 
-    Spacer(modifier = Modifier
+    Spacer(modifier = modifier
         .fillMaxSize()
         .background(backgroundColor)
         // Set strokeWidth based on the size of the viewport
@@ -77,28 +135,52 @@ fun ClockAnimated(
         .drawBehind {
             val halfWidth = size.width / 2
             val halfHeight = size.height / 2
+            val halfStroke = strokeWidth / 2
+            val stepHeight = halfHeight / 12
+
             val center = Offset(x = halfWidth, y = halfHeight)
             val endOffset = Offset(
                 x = halfWidth,
-                y = halfHeight - calculateClockHandLength(halfHeight, currentHour)
+                y = halfHeight - calculateClockHandLength(stepHeight, currentHour)
             )
             // Rotate the line around the pivot point, which is the
             // center of the screen. Rotation goes from 0 to 720 degrees
             rotate(degrees = animationAngle, pivot = center) {
+                // Drawing a clock hand itself
                 drawLine(
                     color = Color.White,
                     start = center,
                     end = endOffset,
                     strokeWidth = strokeWidth,
                 )
+
+                // Drawing a clock hand piece
+                if (assembleValue != -1f) {
+                    val positionY = halfStroke +
+                            calculateAssembleDistance(stepHeight, currentHour) * assembleValue
+
+                    val start = Offset(halfWidth, positionY - halfStroke)
+                    val end = Offset(halfWidth, positionY + halfStroke)
+                    drawLine(
+                        color = Color.White,
+                        start = start,
+                        end = end,
+                        strokeWidth = strokeWidth,
+                    )
+                }
             }
 
             hours.forEach { hour ->
-                if(!dotsVisibility[hour]) return@forEach
+                if (!dotsVisibility[hour]) return@forEach
                 val degree = hour * 30f
                 rotate(degree) {
-                    val start = Offset(x = halfWidth, y = 0f)
-                    val end = Offset(x = halfWidth, strokeWidth)
+                    // Based on the hour value, the travel distance will be longer
+                    val positionY = halfStroke +
+                            stepHeight * hour * (1 - disassembleAnimations[hour].value)
+
+                    val start = Offset(x = halfWidth, y = positionY - halfStroke)
+                    val end = Offset(x = halfWidth, y = positionY + halfStroke)
+
                     drawLine(
                         color = Color.White,
                         start = start,
--- app/src/main/java/com/guilhermemagro/myplayground/ui/screens/ClockSplittingAnimationScreen.kt
+++ app/src/main/java/com/guilhermemagro/myplayground/ui/screens/ClockSplittingAnimationScreen.kt
@@ -1,20 +1,24 @@
 package com.guilhermemagro.myplayground.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.guilhermemagro.myplayground.ui.animations.ClockAnimated
 
 @Composable
 fun ClockSplittingAnimationScreen() {
-    Box(modifier = Modifier.size(300.dp)) {
-        ClockAnimated()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        ClockAnimated(
+            modifier = Modifier.size(300.dp)
+        )
     }
 }
 
